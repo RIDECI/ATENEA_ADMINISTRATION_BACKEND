1368dde4309d24668d238e2dd30a8d23ac096f1c
Robinson Nuñez <robinson.nunez-p@mail.escuelaing.edu.co>
2025-11-11 09:37:01 -0500

    feature - Configuracion CI/CD

--- src/test/java/edu/eci/ATENEA_Administration_BackEnd/AteneaAdministrationBackEndApplicationTests.java
+++ src/test/java/edu/eci/ATENEA_Administration_BackEnd/AteneaAdministrationBackEndApplicationTests.java
@@ -3,7 +3,9 @@ package edu.eci.ATENEA_Administration_BackEnd;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
+})
 class AteneaAdministrationBackEndApplicationTests {
 
 	@Test
